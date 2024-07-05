8cc96b196ef581585784ea6c69a59130e76dafc6
John C. Otilla <johncanila.otilla@bicol-u.edu.ph>
2024-07-05 20:15:33 +0800

    Modal | Animation Timings

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,6 +3,11 @@ import { Transition } from "react-transition-group";
 
 import "./Modal.css";
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000,
+};
+
 const modal = (props) => {
     const cssClasses = [
         "Modal",
@@ -14,7 +19,12 @@ const modal = (props) => {
     ];
 
     return (
-        <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
+        <Transition
+            mountOnEnter
+            unmountOnExit
+            in={props.show}
+            timeout={animationTiming}
+        >
             {(state) => {
                 const cssClasses = [
                     "Modal",
