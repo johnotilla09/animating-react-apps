b8a806d5661fa21abb364dd5a9c149dec2cb14c2
John C. Otilla <johncanila.otilla@bicol-u.edu.ph>
2024-07-05 17:06:35 +0800

    App | CSS  Transition & Animations Limitations

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,15 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>React Animations</h1>
-                <Modal
-                    show={this.state.modalIsOpen}
-                    closed={this.closedModal}
-                />
-                <Backdrop show={this.state.modalIsOpen} />
+                {this.state.modalIsOpen ? (
+                    <Modal
+                        show={this.state.modalIsOpen}
+                        closed={this.closedModal}
+                    />
+                ) : null}
+                {this.state.modalIsOpen ? (
+                    <Backdrop show={this.state.modalIsOpen} />
+                ) : null}
                 <button className="Button" onClick={this.showModal}>
                     Open Modal
                 </button>
